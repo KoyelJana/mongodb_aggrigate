b6b76ebd7ae30168218801bd1a12762f62f304f6
AnurupaDas1505 <anurupadas1505@gmail.com>
2025-08-01 10:32:51 +0530

    crerate a router

---
+++ app/route/testingRouter.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const LogicalController = require('../controller/LogicalController');
+const router=express.Router();
+
+router.post('/employee/',(req,res)=>{
+    res.send('dhgdgdgdg')
+})
+
+module.exports=router
